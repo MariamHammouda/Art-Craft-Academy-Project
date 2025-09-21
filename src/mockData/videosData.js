7d307685430d0ffa3376f6018e862f11555cd338
Mariam Hammouda <mariamhamouda24@gmail.com>
2025-09-21 13:33:42 +0300

    add sample of videos data

---
+++ src/mockData/videosData.js
@@ -0,0 +1,14 @@
+export const videosData = [
+ {
+    url: "https://www.youtube.com/embed/WH37GeN77q8?si=oIetRbdrkhWrTimI&start=2",
+    title: "paper pencil case",
+  },
+  {
+    url: "https://www.youtube.com/embed/JFEui1Ti2F0?si=7TKGURqr5WlYoccS&amp;start=2",
+    title: "Origami a rose from paper ",
+  },
+  {
+    url: "https://www.youtube.com/embed/gjqDo2LcBf4?si=qz84pD1usH7cXSQM&amp;start=2",
+    title: "Making labubu from perler beads",
+  },
+];
